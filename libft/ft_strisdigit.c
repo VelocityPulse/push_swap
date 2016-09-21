91b0f616d18768cdc6cdd27cc9fa53ea320472bc
Clement CHAMEYRAT <velocitypulse@outlook.fr>
2016-09-21 12:43:24 +0200

    mise a jour de la libft

--- libft/ft_strisdigit.c
+++ libft/ft_strisdigit.c
@@ -6,7 +6,7 @@
 /*   By: cchameyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/16 16:16:37 by cchameyr          #+#    #+#             */
-/*   Updated: 2016/06/19 14:53:18 by cchameyr         ###   ########.fr       */
+/*   Updated: 2016/09/21 12:36:43 by cchameyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,15 @@ int		ft_strisdigit(char *str)
 
 	i = -1;
 	end = 0;
-	while (str[i] == ' ')
-		i++;
+	while (str[++i] == ' ')
+		;
 	if (str[i] == '-' || str[i] == '+')
 		i++;
 	while (str[i])
 	{
-		if ((str[i] >= 48 || str[i] <= 57) && end == 0)
+		if ((str[i] >= 48 && str[i] <= 57) && end == 0)
 			i++;
-		else if (str[i] == ' ')
+		else if (str[i] == ' ' && end == 0)
 			end = 1;
 		else
 			return (0);
