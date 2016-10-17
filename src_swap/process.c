e14715738b760a7f4785fbdf1e62245b4047f545
Clement CHAMEYRAT <velocitypulse@outlook.fr>
2016-10-17 13:12:40 +0200

    pret pour travail

--- src_swap/process.c
+++ src_swap/process.c
@@ -6,24 +6,31 @@
 /*   By: cchameyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/14 15:15:15 by cchameyr          #+#    #+#             */
-/*   Updated: 2016/09/22 13:58:17 by cchameyr         ###   ########.fr       */
+/*   Updated: 2016/10/17 13:12:31 by cchameyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-static void		algo(t_pushswap *ps, int i)
+static int		algo_reverse(t_pushswap *ps)
 {
-	if (i < ps->tmpa / 2)
+	int		cpt;
+
+	cpt = 0;
+	while (cpt < ps->tmpa - 1)
 	{
-		while (i--)
-			rule_ra(ps);
+		if (ps->a[cpt] < ps->a[cpt + 1])
+			return (_ERROR_);
+		cpt++;
 	}
-	else
+	while (ps->tmpa > 1)
 	{
-		while (++i <= ps->tmpa)
-			rule_rra(ps);
+		rule_rra(ps);
+		rule_pb(ps);
 	}
+	while (ps->tmpb)
+		rule_pa(ps);
+	return (_SUCCESS_);
 }
 
 void			push_swap(t_pushswap *ps)
@@ -33,18 +40,6 @@ void			push_swap(t_pushswap *ps)
 
 	if (ps->print_display == true || ps->final_display == true)
 		display(ps);
-	min = get_min(ps->a, ps->tmpa);
-	while (min < get_max(ps->a, ps->tmpa))
-	{
-		i = get_index(ps, min);
-		if (get_fault(ps->a, ps->tmpa) == 0)
-			break ;
-		algo(ps, i);
-		if (get_fault(ps->a, ps->tmpa) == 0)
-			break ;
-		rule_pb(ps);
-		min = ps->a[get_index_up(ps, min)];
-	}
-	while (ps->tmpb)
-		rule_pa(ps);
+		if (algo_reverse(ps) == _SUCCESS_)
+			return ;
 }
