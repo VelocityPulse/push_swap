660315061db5610d94149dc436b2bf3c08577620
Clement CHAMEYRAT <velocitypulse@outlook.fr>
2016-10-24 12:57:17 +0200

    correctif -> rule with space

--- src_checker/make_rules.c
+++ src_checker/make_rules.c
@@ -6,7 +6,7 @@
 /*   By: cchameyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/12 13:46:22 by cchameyr          #+#    #+#             */
-/*   Updated: 2016/10/24 12:54:44 by cchameyr         ###   ########.fr       */
+/*   Updated: 2016/10/24 12:56:34 by cchameyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,17 @@
 
 static int		easy_rule(t_checker *c, char *rule)
 {
-	if (ft_strncmp(rule, "ss", 2) == 0)
+	if (ft_strncmp(rule, "ss", 3) == 0)
 	{
 		rule_sa(c);
 		rule_sb(c);
 	}
-	else if (ft_strncmp(rule, "rrr", 3) == 0)
+	else if (ft_strncmp(rule, "rrr", 4) == 0)
 	{
 		rule_rra(c);
 		rule_rrb(c);
 	}
-	else if (ft_strncmp(rule, "rr", 2) == 0)
+	else if (ft_strncmp(rule, "rr", 3) == 0)
 	{
 		rule_ra(c);
 		rule_rb(c);
@@ -36,21 +36,21 @@ static int		easy_rule(t_checker *c, char *rule)
 
 int		select_rule(t_checker *c, char *rule)
 {
-	if (ft_strncmp(rule, "sa", 2) == 0)
+	if (ft_strncmp(rule, "sa", 3) == 0)
 		rule_sa(c);
-	else if (ft_strncmp(rule, "sb", 2) == 0)
+	else if (ft_strncmp(rule, "sb", 3) == 0)
 		rule_sb(c);
-	else if (ft_strncmp(rule, "pa", 2) == 0)
+	else if (ft_strncmp(rule, "pa", 3) == 0)
 		rule_pa(c);
-	else if (ft_strncmp(rule, "pb", 2) == 0)
+	else if (ft_strncmp(rule, "pb", 3) == 0)
 		rule_pb(c);
-	else if (ft_strncmp(rule, "ra", 2) == 0)
+	else if (ft_strncmp(rule, "ra", 3) == 0)
 		rule_ra(c);
-	else if (ft_strncmp(rule, "rb", 2) == 0)
+	else if (ft_strncmp(rule, "rb", 3) == 0)
 		rule_rb(c);
-	else if (ft_strncmp(rule, "rra", 3) == 0)
+	else if (ft_strncmp(rule, "rra", 4) == 0)
 		rule_rra(c);
-	else if (ft_strncmp(rule, "rrb", 3) == 0)
+	else if (ft_strncmp(rule, "rrb", 4) == 0)
 		rule_rrb(c);
 	else if (easy_rule(c, rule) == _SUCCESS_)
 		;
